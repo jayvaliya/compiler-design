8d08ee1902d65b04968d27f4480a4646909f4652
jay valiya <valiyajay555@gmail.com>
2024-12-04 16:27:59 +0530

    updated practical 1

--- practical1.c
+++ practical1.c
@@ -1,22 +1,93 @@
-// Practical 1: Program to implement lexicla analyzer in c.
-#include <stdio.h>
+// Practical 1: Program to implement lexiclal analyzer in c.
+
 #include <ctype.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 100;
+
+bool isOperator(char c)
+{
+    if (c == '+' || c == '-' || c == '*' || c == '/' || c == '^')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isIdentifier(char c)
+{
+    if (isalpha(c) || c == '_')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isSpecialSymbol(char c)
+{
+    if (c == '(' || c == ')' || c == '{' || c == '}' || c == '[' || c == ']' || c == ',' || c == ';' || c == ':' || c == '.')
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isConstant(char c)
+{
+    if (isdigit(c))
+    {
+        return true;
+    }
+    return false;
+}
+
+bool isKeywords(char c)
+{
+    char keywords[32][10] = {
+        "auto", "double", "int", "struct", "break", "else", "long", "switch", "case", "enum", "register", "typedef", "char", "extern", "return", "union", "const", "float", "short", "unsigned", "continue", "for", "signed", "void", "default", "goto", "sizeof", "volatile", "do", "if", "static", "while"};
+    for (int i = 0; i < 32; i++)
+    {
+        if (strcmp(keywords[i], c) == 0)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main()
 {
-    char ch;
-    printf("Enter a character: ");
-    scanf("%c", &ch);
-    if (isalpha(ch))
+    char c;
+    char str[100];
+    int i = 0;
+    printf("Enter a string: ");
+    gets(str);
+    while (str[i] != '\0')
     {
-        printf("It is a letter.\n");
-    }
-    else if (isdigit(ch))
-    {
-        printf("It is a digit.\n");
-    }
-    else
-    {
-        printf("It is a special character.\n");
+        c = str[i];
+        if (isOperator(c))
+        {
+            printf("%c is an operator\n", c);
+        }
+        else if (isIdentifier(c))
+        {
+            printf("%c is an identifier\n", c);
+        }
+        else if (isSpecialSymbol(c))
+        {
+            printf("%c is a special symbol\n", c);
+        }
+        else if (isConstant(c))
+        {
+            printf("%c is a constant\n", c);
+        }
+        else if (isKeywords(c))
+        {
+            printf("%c is a keyword\n", c);
+        }
+        i++;
     }
     return 0;
 }
